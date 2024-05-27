55acfb02cf8311f7066d9341d93a36d1ab5b6613
WINDOWS <WINDOWS@WINDOWS-PC>
2024-05-27 09:41:02 +0700

    Hoan thien code bai 4 vi du 1

--- src/bai04/vidu1/QuanLyNhanSu.java
+++ src/bai04/vidu1/QuanLyNhanSu.java
@@ -5,8 +5,10 @@ public class QuanLyNhanSu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SinhVien sv = new SinhVien("Ngoc Truong", 2005, "IT024324",
-				"Cong nghe thong tin");
-		System.out.print(sv);
+				"               ");
+		System.out.println(sv);
+		GiangVien gv = new GiangVien("Chau Dung", 1978, "GV1234", "CNTT", "ThS");
+		System.out.println(gv);
 	}
 
 }
