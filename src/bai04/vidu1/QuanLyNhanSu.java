3e8f4146c60a78ebcb670eafff1aaf79dc6cfcd4
WINDOWS <WINDOWS@WINDOWS-PC>
2024-05-27 09:16:04 +0700

    Hay hoan thien tiep lop GiangVien cho bai 4 vi du 1

---
+++ src/bai04/vidu1/QuanLyNhanSu.java
@@ -0,0 +1,12 @@
+package bai04.vidu1;
+
+public class QuanLyNhanSu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien sv = new SinhVien("Ngoc Truong", 2005, "IT024324",
+				"Cong nghe thong tin");
+		System.out.print(sv);
+	}
+
+}
