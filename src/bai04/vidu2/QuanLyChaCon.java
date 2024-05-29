c35542a102c441fb801246809f09a15ab12650c3
WINDOWS <WINDOWS@WINDOWS-PC>
2024-05-29 07:27:49 +0700

    Bai 4 - vi du 2 - lan 2

---
+++ src/bai04/vidu2/QuanLyChaCon.java
@@ -0,0 +1,15 @@
+package bai04.vidu2;
+
+public class QuanLyChaCon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LopCha obj1 = new LopCha("Lop Cha", 15, 3.1417);
+		obj1.thuoctinh2 = 16;
+		obj1.thuoctinh3 = 7.5;
+		System.out.println(obj1);
+		obj1.phuongthuc2();
+		obj1.phuongthuc3();
+	}
+
+}
