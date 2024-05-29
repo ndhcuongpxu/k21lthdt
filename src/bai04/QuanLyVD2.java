47107407f0290a65a923ab8cc0397eb775b2605a
WINDOWS <WINDOWS@WINDOWS-PC>
2024-05-29 07:54:36 +0700

    Bai 4 - vi du 2 - lan 4 - minh hoa ko truy cap dc protected tu package khac

---
+++ src/bai04/QuanLyVD2.java
@@ -0,0 +1,13 @@
+package bai04;
+
+import bai04.vidu2.LopCha;
+
+public class QuanLyVD2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LopCha obj1 = new LopCha("Son", 100, 9.9);
+		obj1.phuongthuc3();
+	}
+
+}
