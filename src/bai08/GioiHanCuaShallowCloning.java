3464ab00e7d5841da3f5397e919380db71ed2a52
WINDOWS <WINDOWS@WINDOWS-PC>
2024-06-10 09:03:24 +0700

    bai 8 - vi du 3 - gio han cua shallow cloning

---
+++ src/bai08/GioiHanCuaShallowCloning.java
@@ -0,0 +1,32 @@
+package bai08;
+
+import java.util.ArrayList;
+
+import bai08.vidu3.HoaDon;
+import bai08.vidu3.SanPham;
+
+public class GioiHanCuaShallowCloning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<SanPham> dssp = new ArrayList<SanPham>();
+		dssp.add(new SanPham(1, "Nokia"));
+		dssp.add(new SanPham(2, "SamSung"));
+		HoaDon hd1 = new HoaDon(1, dssp);
+		System.out.println(hd1);
+		//Thuc hien shallow cloning
+		HoaDon hd2;
+		try {
+			hd2 = (HoaDon)hd1.clone();
+			hd2.setMaHoaDon(15);
+			hd2.getDssp().getFirst().setTenSanPham("Iphone");
+			System.out.println(hd1);
+			System.out.println(hd2);		
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
