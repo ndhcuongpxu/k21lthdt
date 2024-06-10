957706fa600037428d421ad029c4c3b72d2baaf7
WINDOWS <WINDOWS@WINDOWS-PC>
2024-06-10 09:20:23 +0700

    Bai 8 - vi du 4 - deep cloning

---
+++ src/bai08/vidu4/SanPham.java
@@ -0,0 +1,44 @@
+package bai08.vidu4;
+
+public class SanPham {
+	private int maSanPham;
+	private String tenSanPham;
+	/**
+	 * @param maSanPham
+	 * @param tenSanPham
+	 */
+	public SanPham(int maSanPham, String tenSanPham) {
+		super();
+		this.maSanPham = maSanPham;
+		this.tenSanPham = tenSanPham;
+	}
+	/**
+	 * @return the maSanPham
+	 */
+	public int getMaSanPham() {
+		return maSanPham;
+	}
+	/**
+	 * @param maSanPham the maSanPham to set
+	 */
+	public void setMaSanPham(int maSanPham) {
+		this.maSanPham = maSanPham;
+	}
+	/**
+	 * @return the tenSanPham
+	 */
+	public String getTenSanPham() {
+		return tenSanPham;
+	}
+	/**
+	 * @param tenSanPham the tenSanPham to set
+	 */
+	public void setTenSanPham(String tenSanPham) {
+		this.tenSanPham = tenSanPham;
+	}
+	@Override
+	public String toString() {
+		return "SanPham [maSanPham=" + maSanPham + ", tenSanPham=" + tenSanPham + "]";
+	}
+	
+}
